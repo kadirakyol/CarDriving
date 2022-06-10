fcd08260cfe7036b91ea2214205c041ab8824b36
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-10 11:37:18 +0300

    some codes

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSec = 1f;
-    [SerializeField] private float turnSpeed = 100f;
+    [SerializeField] private float turnSpeed = 200f;
 
     private int steerValue;
     
@@ -17,6 +19,14 @@ public class Car : MonoBehaviour
         transform.Rotate(0f,steerValue * turnSpeed * Time.deltaTime,0f);
         
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(0);
+        }
         
     }
 
